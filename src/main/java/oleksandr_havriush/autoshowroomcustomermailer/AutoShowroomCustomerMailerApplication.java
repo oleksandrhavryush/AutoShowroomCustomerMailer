a1d4f85bee014a635a1349d56cc60c13da9c64e4
oleksandrhavryush <havryush@gmail.com>
2024-06-03 14:13:43 +0200

    Initial commit

---
+++ src/main/java/oleksandr_havriush/autoshowroomcustomermailer/AutoShowroomCustomerMailerApplication.java
@@ -0,0 +1,13 @@
+package oleksandr_havriush.autoshowroomcustomermailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoShowroomCustomerMailerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoShowroomCustomerMailerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oleksandr_havriush/autoshowroomcustomermailer/AutoShowroomCustomerMailerApplicationTests.java
@@ -0,0 +1,13 @@
+package oleksandr_havriush.autoshowroomcustomermailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoShowroomCustomerMailerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
