77300e642182d6d2bb2b1bcc64915a3113146ee4
oleksandrhavryush <havryush@gmail.com>
2024-06-07 16:46:34 +0200

    Add client registration feature
    - Developed functionality for creating new clients via console.
    - Client data is now persistable in the database.
    - Included basic validation for client attributes, such as 5-digit numeric postal codes for German clients.

---
+++ src/main/java/oleksandr_havriush/autoshowroomcustomermailer/model/Address.java
@@ -0,0 +1,40 @@
+package oleksandr_havriush.autoshowroomcustomermailer.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "address")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "house_number")
+    @Pattern(regexp = "^[0-9a-zA-Z-/]*$", message = "House number is invalid")
+    private String houseNumber;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "postal_code")
+    @NotNull(message = "{customer.create.errors.postalCode_is_null}")
+    @Pattern(regexp = "^\\d{5}$", message = "{customer.create.errors.postalCode_is_invalid}")
+    private String postalCode;
+
+
+    @Column(name = "country")
+    private String country;
+}
---
+++ src/main/java/oleksandr_havriush/autoshowroomcustomermailer/model/Customer.java
@@ -0,0 +1,27 @@
+package oleksandr_havriush.autoshowroomcustomermailer.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Address address;
+}
